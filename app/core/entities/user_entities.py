e32e379f5847aedda72bb982cc707a59a9ede570
Suspir0n <evandrojunior1615@gmail.com>
2024-01-06 13:44:23 -0300

    :recycle: refactor: updating code in tests of entitie user, removing field uuid

--- app/core/entities/user_entities.py
+++ app/core/entities/user_entities.py
@@ -1,6 +1,5 @@
 class User:
-    def __init__(self, uuid, username, first_name, last_name, email, password):
-        self.uuid = uuid
+    def __init__(self, username, first_name, last_name, email, password):
         self.username = username
         self.first_name = first_name
         self.last_name = last_name
